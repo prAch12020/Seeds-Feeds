7ca5cd8102f908a624bfd910246233434eeaa18c
prAch12020 <65279447+prAch12020@users.noreply.github.com>
2023-10-27 14:39:44 +0300

    Update address.php

--- data/address.php
+++ data/address.php
@@ -5,11 +5,11 @@ $date = $_POST['country'];
 
 $address =$vet_add .", ". $msg .", ". $date; ; // Google HQ
 $prepAddr = str_replace(' ','+',$address);
-echo $prepAddr;
+
 $geocode=file_get_contents('https://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false&key=AIzaSyClL3XpKG_N2NuCvin3bAX11M8ZqEh-Fig');
-$output= json_decode($geocode);
-$latitude = $output->results[0]->geometry->location->lat;
-$longitude = $output->results[0]->geometry->location->lng;
+$output= json_decode($geocode, true);
+$latitude = $output['results'][0]['geometry']['location']['lat'];
+$longitude = $output['results'][0]['geometry']['location']['lng'];
 	
 echo "latitude - ".$latitude;
 echo "longitude - ".$longitude;
