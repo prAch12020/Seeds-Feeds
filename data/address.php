1168506f14985be78c87baee3f167a0d77bbbf7c
prAch12020 <65279447+prAch12020@users.noreply.github.com>
2023-10-27 15:27:00 +0300

    Update address.php

--- data/address.php
+++ data/address.php
@@ -1,25 +1,22 @@
 <?php 
 $vet_add = $_POST['add'];
-
 $msg = $_POST['city'];
 $date = $_POST['country'];
 
 $address =$vet_add .", ". $msg .", ". $date; 
-echo $address;
 $prepAddr = urlencode($address);
-echo $prepAddr;
-
 $url = 'https://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&key=AIzaSyClL3XpKG_N2NuCvin3bAX11M8ZqEh-Fig';
-
-echo $url;
 $resp = json_decode( file_get_contents( $url ), true );
-echo $resp;
-// Latitude and Longitude (PHP 7 syntax)
-$lat    = $resp['results'][0]['geometry']['location']['lat'] ?? '';
-$long   = $resp['results'][0]['geometry']['location']['lng'] ?? '';
-echo "latitude - ".$lat;
-echo "longitude - ".$lng;
 
+if ($data && isset($data['results'][0]['geometry']['location']['lat']) && isset($data['results'][0]['geometry']['location']['lng'])) {
+    $lat = $data['results'][0]['geometry']['location']['lat'];
+    $lng = $data['results'][0]['geometry']['location']['lng'];
+    echo "latitude - ".$lat;
+    echo "longitude - ".$lng;
+}
+else{
+  echo "Couldn't retrive coordinates";
+}
 
 require_once 'config.php';
 saveAddress($vet_add, $msg, $date, $lat, $lng);
