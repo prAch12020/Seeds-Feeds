ead78c3936ba70eba6d7b3c30c24d56923e90361
prAch12020 <65279447+prAch12020@users.noreply.github.com>
2023-10-27 15:31:49 +0300

    Update address.php

--- data/address.php
+++ data/address.php
@@ -4,19 +4,24 @@ $msg = $_POST['city'];
 $date = $_POST['country'];
 
 $address =$vet_add .", ". $msg .", ". $date; 
-$prepAddr = urlencode($address);
+$prepAddr = urlencode(str_replace(' ', '+', $address));
+
 $url = 'https://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&key=AIzaSyClL3XpKG_N2NuCvin3bAX11M8ZqEh-Fig';
 $data = json_decode( file_get_contents( $url ), true );
 
-if ($data && isset($data['results'][0]['geometry']['location']['lat']) && isset($data['results'][0]['geometry']['location']['lng'])) {
+var_dump($data); // Debug: Inspect the entire JSON response
+
+if ($data['status'] === 'OK') {
+    // Successfully retrieved coordinates
     $lat = $data['results'][0]['geometry']['location']['lat'];
     $lng = $data['results'][0]['geometry']['location']['lng'];
-    echo "latitude - ".$lat;
-    echo "longitude - ".$lng;
+    echo "latitude - " . $lat;
+    echo "longitude - " . $lng;
+} else {
+    // Handle the case where the API response has an error
+    echo "Geocoding API Error: " . $data['status'];
 }
-else{
-  echo "Couldn't retrive coordinates";
-}
+
 
 require_once 'config.php';
 saveAddress($vet_add, $msg, $date, $lat, $lng);
